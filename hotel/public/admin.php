0e984713b2df50d0588e942f1614dfe9f0e7adba
jbbertt <31472515+jbbertt@users.noreply.github.com>
2017-10-28 20:25:18 +0800

    Working on admin php

---
+++ hotel/public/admin.php
@@ -0,0 +1,33 @@
+
+</!DOCTYPE html PUBLIC>
+<html>
+<head>
+	<title>Admin Page</title>
+	<link href="stylesheets/public.css" media="all" rel="stylesheets" type="text/css">
+</head>
+
+<body>
+	<div id ="header">
+		<h1>Hotel Booking and Reservation System</h1>
+	</div>
+		<div id ="main">
+			<div id ="navigation">
+				<a href="manage_cotent.php">HOME</a>
+				<a href="manage_admins.php">ABOUT US</a>
+				<a href="logout.php">ADMINSTRATION</a>
+				<a href="logout.php">REPORTS</a>
+				<a href="logout.php">CHANGE PASSWORD</a>
+				<a href="logout.php">LOGOUT</a>
+
+			</div>
+			<div id ="page">
+				<p>Page ni dre</p>
+			</div>
+		</div>
+
+	<div id ="footer">
+		Copyright 2017, Viernes Corp
+	</div>
+
+</body>
+</html>
---
+++ hotel/public/manage_content.php
@@ -0,0 +1,33 @@
+
+</!DOCTYPE html PUBLIC>
+<html>
+<head>
+	<title>Manage Content</title>
+	<link rel="stylesheet" type="text/css" href="hotel/public/stylesheets/public.css">
+</head>
+
+<body>
+	<div id ="header">
+		<h1>Hotel Booking and Reservation System</h1>
+	</div>
+		<div id ="main">
+			<div id ="navigation">
+				<a href="manage_cotent.php">HOME</a>
+				<a href="manage_admins.php">ABOUT US</a>
+				<a href="logout.php">ADMINSTRATION</a>
+				<a href="logout.php">REPORTS</a>
+				<a href="logout.php">CHANGE PASSWORD</a>
+				<a href="logout.php">LOGOUT</a>
+
+			</div>
+			<div id ="page">
+				<p>Page ni dre</p>
+			</div>
+		</div>
+
+	<div id ="footer">
+		Copyright 2017 &copy; Jazzy Bert Viernes
+	</div>
+
+</body>
+</html>
